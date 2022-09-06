49d6fcd98178e6559049063e58d539dc486ce85d
Lewis <a046829713@gmail.com>
2022-09-06 16:34:59 +0800

    1st commit

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import Crawler_Service
+
+
+def main():
+    service = Crawler_Service.Crawler_Service()
+    # service.get_data()
+    service.get_daydata()
+
+
+if __name__ == '__main__':
+    main()
